07abeb316550c9733290dd4176ff5d331989e4db
akopylov <akopylov@nokogerra.lab>
2022-10-18 16:45:34 +0300

    fix

--- ansible/useful_commands.sh
+++ ansible/useful_commands.sh
@@ -14,6 +14,17 @@ kubectl get pods --all-namespaces --watch
 #get cluster nodes
 kubectl get nodes
 
+#list existing tokens on controller
+kubeadm token list
+
+#create token
+kubeadm token create
+kubeadm token create --print-join-command
+
+#show cert hash
+openssl x509 -pubkey -in /etc/kubernetes/pki/ca.crt | openssl rsa -pubin -outform der 2>/dev/null | openssl dgst -sha256 -hex | sed 's/^.* //'
+
+
 #install kubectl autocompleteion
 #https://kubernetes.io/docs/tasks/tools/included/optional-kubectl-configs-bash-linux/
 echo 'source <(kubectl completion bash)' >>~/.bashrc
